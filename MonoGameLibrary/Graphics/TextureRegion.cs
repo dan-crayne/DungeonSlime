b956da07858ba1b19ea197849f5316328fe42623
Dan Crayne <dan@crayne.net>
2025-09-22 12:16:16 -0700

    Customize UI

--- MonoGameLibrary/Graphics/TextureRegion.cs
+++ MonoGameLibrary/Graphics/TextureRegion.cs
@@ -112,4 +112,24 @@ public class TextureRegion
         );
     }
 
+    /// <summary>
+    /// Gets the top normalized texture coordinate of this region.
+    /// </summary>
+    public float TopTextureCoordinate => SourceRectangle.Top / (float)Texture.Height;
+
+    /// <summary>
+    /// Gets the bottom normalized texture coordinate of this region.
+    /// </summary>
+    public float BottomTextureCoordinate => SourceRectangle.Bottom / (float)Texture.Height;
+
+    /// <summary>
+    ///  Gets the left normalized texture coordinate of this region.
+    /// </summary>
+    public float LeftTextureCoordinate => SourceRectangle.Left / (float)Texture.Width;
+
+    /// <summary>
+    /// Gets the right normalized texture coordinate of this region.
+    /// </summary>
+    public float RightTextureCoordinate => SourceRectangle.Right / (float)Texture.Width;
+
 }
